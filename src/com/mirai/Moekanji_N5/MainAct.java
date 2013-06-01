ff0600d8e37f58d6eb8597e84c144a743c2a2d90
Weroth <weroth@gmail.com>
2013-06-01 11:18:34 +0800

    Initial android project commit

---
+++ src/com/mirai/Moekanji_N5/MainAct.java
@@ -0,0 +1,15 @@
+package com.mirai.Moekanji_N5;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainAct extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
